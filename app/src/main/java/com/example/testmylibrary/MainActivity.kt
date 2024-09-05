06ac6bc89b93ea7db22b08cc852c9f5946dfcbc8
flavioroldan <flavio_roldan@hotmail.com>
2024-09-05 11:54:41 +0100

    Updated myLibrary version to 1.2.0 and optiized imports

--- app/src/main/java/com/example/testmylibrary/MainActivity.kt
+++ app/src/main/java/com/example/testmylibrary/MainActivity.kt
@@ -24,7 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mylibrary.model.Service
 import com.example.mylibrary.model.request.SaveStringRequestData
